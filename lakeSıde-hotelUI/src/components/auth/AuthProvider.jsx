b90834a4b6df3ad076a9821fcf1c17733a2d1a31
seval2016 <sevalsenturk2016@gmail.com>
2024-12-08 23:55:03 +0300

    lakeSide-hotelUI

---
+++ lakeSıde-hotelUI/src/components/auth/AuthProvider.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const AuthProvider = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default AuthProvider; // Burada 'export default' kullanıyoruz
