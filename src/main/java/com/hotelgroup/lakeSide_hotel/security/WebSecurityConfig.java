2f8c4e8d58326e53a9869cd4b145fb5f7f5a265e
seval2016 <sevalsenturk2016@gmail.com>
2024-12-20 01:51:00 +0300

    version 1.0.4

--- src/main/java/com/hotelgroup/lakeSide_hotel/security/WebSecurityConfig.java
+++ src/main/java/com/hotelgroup/lakeSide_hotel/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.hotelgroup.lakeSide_hotel.security;
 
 import com.hotelgroup.lakeSide_hotel.security.jwt.AuthTokenFilter;
 import com.hotelgroup.lakeSide_hotel.security.jwt.JwtEntryPoint;
+
 import com.hotelgroup.lakeSide_hotel.security.user.HotelUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +12,14 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
--- src/main/java/com/hotelgroup/lakeSide_hotel/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/hotelgroup/lakeSide_hotel/security/jwt/AuthTokenFilter.java
@@ -5,9 +5,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,21 +19,20 @@ import java.io.IOException;
 /**
  * @author seval
  */
-@Component
-@RequiredArgsConstructor
 @Slf4j
-public class AuthTokenFilter extends OncePerRequestFilter {
+public class AuthTokenFilter  extends OncePerRequestFilter {
 
-    private final JwtUtils jwtUtils;  // Constructor Injection
-    private final HotelUserDetailsService userDetailsService;  // Constructor Injection
-
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private HotelUserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
         try {
             String jwt = parseJwt(request);
-            if (jwt != null && jwtUtils.validateToken(jwt)) {
+            if(jwt != null && jwtUtils.validateToken(jwt)){
                 String email = jwtUtils.getUsernameForToken(jwt);
                 UserDetails userDetails = userDetailsService.loadUserByUsername(email);
                 var authenticationToken =
@@ -42,15 +40,15 @@ public class AuthTokenFilter extends OncePerRequestFilter {
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
-        } catch (Exception e) {
-            log.error("Cannot set user authentication: {}", e.getMessage());
+        } catch (Exception e){
+            log.error("Cannot set user authentication: {}",  e.getMessage());
         }
-        filterChain.doFilter(request, response);
+        filterChain.doFilter(request,response);
     }
 
     private String parseJwt(HttpServletRequest request) {
         String headerAuth = request.getHeader("Authorization");
-        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer")) {
+        if(StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer")){
             return headerAuth.substring(7);
         }
         return null;
