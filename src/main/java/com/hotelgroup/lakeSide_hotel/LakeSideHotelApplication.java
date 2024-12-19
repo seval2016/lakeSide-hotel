26b1d089628f6e822255a2db316e3126972a1633
seval2016 <sevalsenturk2016@gmail.com>
2024-12-20 01:12:51 +0300

    version 1.0.4

--- src/main/java/com/hotelgroup/lakeSide_hotel/LakeSideHotelApplication.java
+++ src/main/java/com/hotelgroup/lakeSide_hotel/LakeSideHotelApplication.java
@@ -3,7 +3,7 @@ package com.hotelgroup.lakeSide_hotel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.hotelgroup.lakeSide_hotel")
 public class LakeSideHotelApplication {
 
 	public static void main(String[] args) {
